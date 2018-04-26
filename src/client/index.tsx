83cfc49d6b35980f86ca9d7654acd5939d311c67
Pankaj Soni <pankajsoni91@gmail.com>
2018-04-26 09:44:42 +0530

    update the store name to configure store

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,12 +3,12 @@ import { render } from 'react-dom';
 import App from './routes/Routes';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store/configureStore';
+import configureStore from './redux/store/configureStore';
 
 const rootEl = document.getElementById('root');
 
 render(
-  <Provider store={store()}>
+  <Provider store={configureStore()}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
