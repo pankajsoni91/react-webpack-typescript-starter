0d2272835596f505b9707e6b5a8a166b7d96b4ff
Pankaj Soni <pankajsoni91@gmail.com>
2018-05-11 10:51:28 +0530

    added the test cases

---
+++ tests/App.test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+// import App from '../src//App';
+
+it('Test setup test', () => {
+  expect(1).toEqual(1);
+});
